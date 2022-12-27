2e834392b2e21d5c7cda328d13cb73b87fe548a6
Mesquita <mesquita@aospa.co>
2022-12-27 11:06:22 -0300

    crawler: initilize tests with Spock
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- crawler/src/test/groovy/crawler/ConvenioControllerSpec.groovy
+++ crawler/src/test/groovy/crawler/ConvenioControllerSpec.groovy
@@ -11,8 +11,14 @@ class ConvenioControllerSpec extends Specification implements ControllerUnitTest
     def cleanup() {
     }
 
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "verifica o retorno"() {
+        def result = [
+                "/home/mesquita/ZG-MVP/crawler/downloads/demonstrativos/1_convenio_associacaoMedica_2017-11-27_55084598.txt.csv",
+                "/home/mesquita/ZG-MVP/crawler/downloads/demonstrativos/2_convenio_associacaoMedica_2017-12-10_50131802.txt.csv",
+                "/home/mesquita/ZG-MVP/crawler/downloads/demonstrativos/3_convenio_associacaoMedica_2018-01-10.txt.csv",
+                "/home/mesquita/ZG-MVP/crawler/downloads/demonstrativos/4_convenio_associacaoMedica_2018-02-12.txt.csv"
+        ]
+
+        expect: result.size() == 4
     }
 }
